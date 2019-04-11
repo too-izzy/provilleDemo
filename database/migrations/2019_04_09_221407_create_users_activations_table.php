fe651fce1627686d67e30ed3e3e9fd3caea25fda
too-izzy <izzyuzoma@gmail.com>
2019-04-11 10:23:46 +0100

    Commit - provilleDemo Project to Send A Verification Link To Mail During Registration Using Laravel Framework

---
+++ database/migrations/2019_04_09_221407_create_users_activations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersActivationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_activations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->string('token');
+            $table->timestamp('create_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+
+        schema::table('users', function(Blueprint $table){
+            $table->boolean('is_activated')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_activations');
+
+        schema::table('users', function(Blueprint $table){
+            $table->dropColumn('is_activated');
+        });
+    }
+}
---
+++ resources/views/mail/activation.blade.php
@@ -0,0 +1,6 @@
+<h1>ProvilleDemo Activation</h1>
+
+Wellcome {{ $name  }}
+
+Please Activate Your Account : {{ url('user/activation', $link) }}
+
